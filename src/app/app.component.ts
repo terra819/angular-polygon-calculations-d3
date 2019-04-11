e7efbeac8cc73ba7bf16286d6c5ba41cf77fd19f
terra819 <mrmrsgilbert@gmail.com>
2019-04-11 12:02:47 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,43 +9,29 @@ import * as d3 from 'd3';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  svg: any;
-  points: [] = [];
+  // svg: any;
+  points = [];
   dragging = false;
   drawing = false;
   startPoint: any;
   g: any;
-  dragger: any;
 
-  constructor() {
-    // let results = d3.select('#results');
-    this.svg = d3.select('svg');
-    //   .attr('height', 200)
-    //   .attr('width', 500);
-
-    this.dragger = d3.drag()
-      // .on('drag', this.handleDrag(this))
-      .on('end', function (d) {
-        this.dragging = false;
-      });
+  handleDrag() {
+    const svg = d3.select('svg');
+    if (this.drawing) return;
+    let dragCircle = d3.select(this), newPoints = [], circle;
+    this.dragging = true;
+    let poly = svg.select('polygon');
+    let circles = svg.selectAll('circle');
+    // dragCircle
+    //   .attr('cx', event.x)
+    //   .attr('cy', event.y);
+    for (let i = 0; i < circles[0].length; i++) {
+      circle = d3.select(circles[0][i]);
+      newPoints.push([circle.attr('cx'), circle.attr('cy')]);
+    }
+    poly.attr('points', newPoints);
   }
-
-  // handleDrag(dragee) {
-  //   if (this.drawing) return;
-  //   let dragCircle = d3.select(this), newPoints = [], circle;
-  //   this.dragging = true;
-  //   let poly = d3.select(dragee.parentNode).select('polygon');
-  //   let circles = d3.select(dragee.parentNode).selectAll('circle');
-  //   d3.getEvent = function(){return require("d3-selection").event}.bind(this);
-  //   dragCircle
-  //     .attr('cx', d3.getEvent().x)
-  //     .attr('cy', d3.getEvent().y);
-  //   for (let i = 0; i < circles[0].length; i++) {
-  //     circle = d3.select(circles[0][i]);
-  //     newPoints.push([circle.attr('cx'), circle.attr('cy')]);
-  //   }
-  //   poly.attr('points', newPoints);
-  // }
 
   getRandomColor() {
     let letters = '0123456789ABCDEF'.split('');
@@ -59,14 +45,15 @@ export class AppComponent {
   handleMouseUp(event: any) {
     if (this.dragging) return;
     this.drawing = true;
-    this.startPoint = [event.clientX, event.clientY];
-    if (this.svg.select('g.drawPoly').empty()) this.g = this.svg.append('g').attr('class', 'drawPoly');
-    
+    this.startPoint = [event.offsetX, event.offsetY];
+    const svg = d3.select('svg');
+    if (svg.select('g.drawPoly').empty()) this.g = svg.append('g').attr('class', 'drawPoly');
+
     if (event.target.hasAttribute('is-handle')) {
-      this.closePolygon();
+      this.closePolygon(event);
       return;
     };
-    // this.points.push(d3.mouse(this));
+    this.points.push(this.startPoint);
     this.g.select('polyline').remove();
     let polyline = this.g.append('polyline').attr('points', this.points)
       .style('fill', 'none')
@@ -90,16 +77,22 @@ export class AppComponent {
     let line = g.append('line')
       .attr('x1', this.startPoint[0])
       .attr('y1', this.startPoint[1])
-      .attr('x2', d3.mouse(this)[0] + 2)
-      .attr('y2', d3.mouse(this)[1])
+      .attr('x2', event.offsetX)
+      .attr('y2', event.offsetY)
       .attr('stroke', '#53DBF3')
       .attr('stroke-width', 1);
     //calculate line length
   }
 
-  closePolygon() {
-    this.svg.select('g.drawPoly').remove();
-    this.g = this.svg.append('g');
+  closePolygon(event: any) {
+    const dragger = d3.drag()
+      .on('drag', this.handleDrag)
+      .on('end', function (d) {
+        this.dragging = false;
+      });
+    const svg = d3.select('svg');
+    svg.select('g.drawPoly').remove();
+    this.g = svg.append('g');
     this.g.append('polygon')
       .attr('points', this.points)
       .style('fill', this.getRandomColor());
@@ -114,8 +107,10 @@ export class AppComponent {
         .attr('fill', '#FDBC07')
         .attr('stroke', '#000')
         .attr('is-handle', 'true')
-        // .style({cursor: 'move'})
-        .call(this.dragger);
+        .style({ cursor: 'move' });
+        console.log(circle);
+        circle
+        .call(dragger);
     }
     this.points.splice(0);
     this.drawing = false;

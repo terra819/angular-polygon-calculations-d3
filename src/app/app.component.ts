273e9172d30619bea4bddd79dd1cbd497b8aa105
terra819 <mrmrsgilbert@gmail.com>
2019-04-16 15:56:30 -0400

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import * as d3 from 'd3';
+import { Shape } from './shape';
+const uuidV4 = require('uuid/v4');
 // adapted from
 // http://jsfiddle.net/eZQdE/43/
 
@@ -8,19 +10,17 @@ import * as d3 from 'd3';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  shapes = {};
-  points = [];
+export class AppComponent implements AfterViewInit {
+  shapes = [];
   dragging = false;
-  drawing = false;
+  drawing: Shape;
   startPoint: any;
   g: any;
+  svg: any;
 
-
-
-
-
-
+  ngAfterViewInit() {
+    this.svg = d3.select('svg');
+  }
 
   handleMouseMove(event: any) {
     if (!this.drawing) return;
@@ -48,43 +48,37 @@ export class AppComponent {
 
   handleMouseUp(event: any) {
     if (this.dragging) return;
-    this.drawing = true;
+    // this.drawing = true;
     this.startPoint = [event.offsetX, event.offsetY];
-    const svg = d3.select('svg');
-    if (svg.select('g.drawPoly').empty()) this.g = svg.append('g').attr('class', 'drawPoly');
+
+    if (this.svg.select('g.drawPoly').empty() || !this.drawing) {
+      const shape = new Shape();
+      shape.id = uuidV4();
+      this.g = this.svg.append('g').attr('class', 'drawPoly').attr('id', shape.id);
+      this.shapes.push(shape);
+      this.drawing = shape;
+    }
 
 
-    this.points.push(this.startPoint);
+    this.drawing.points.push(this.startPoint);
     this.g.select('polyline').remove();
-    let polyline = this.g.append('polyline').attr('points', this.points)
+    let polyline = this.g.append('polyline').attr('points', this.drawing.points)
       .style('fill', 'none')
       .attr('stroke', '#000');
     this.g.select('text.tempLabel').remove();
-   
-    for (let i = 0; i < this.points.length; i++) {
+
+    for (let i = 0; i < this.drawing.points.length; i++) {
       this.g.append('circle')
-        .attr('cx', this.points[i][0])
-        .attr('cy', this.points[i][1])
+        .attr('cx', this.drawing.points[i][0])
+        .attr('cy', this.drawing.points[i][1])
         .attr('r', 4)
         .attr('fill', 'yellow')
         .attr('stroke', '#000')
         .attr('is-handle', 'true')
         .style({ cursor: 'pointer' });
-
-      if (i > 0) {
-        this.g.selectAll('text.lineLabel').remove();
-        //calculate line length
-        const midPoint = { x: (this.points[i - 1][0] + this.points[i][0]) / 2, y: (this.points[i - 1][1] + this.points[i][1]) / 2 };
-        const t = Math.round(polyline.node().getTotalLength() * 100) / 100;
-        let text = this.g.insert('text', ':first-child')
-          .attr('x', midPoint.x)
-          .attr('y', midPoint.y)
-          .attr('text-anchor', 'middle')
-          .attr('class', 'lineLabel')
-          .text(t);
-      }
     }
- if (event.target.hasAttribute('is-handle')) {
+    this.updateLabels();
+    if (event.target.hasAttribute('is-handle')) {
       this.closePolygon(event);
       return;
     };
@@ -117,16 +111,16 @@ export class AppComponent {
     // svg.select('g.drawPoly').remove();
     const g = svg.insert('g', ':first-child')
     g.append('polygon')
-      .attr('points', this.points)
+      .attr('points', this.drawing.points)
       .style('fill', 'FFF');
     g.selectAll('circles').remove();
-    for (var i = 0; i < this.points.length; i++) {
+    for (var i = 0; i < this.drawing.points.length; i++) {
       var circle = g.selectAll('circles')
-        .data([this.points[i]])
+        .data([this.drawing.points[i]])
         .enter()
         .append('circle')
-        .attr('cx', this.points[i][0])
-        .attr('cy', this.points[i][1])
+        .attr('cx', this.drawing.points[i][0])
+        .attr('cy', this.drawing.points[i][1])
         .attr('r', 4)
         .attr('fill', '#FDBC07')
         .attr('stroke', '#000')
@@ -136,6 +130,34 @@ export class AppComponent {
     }
 
     // this.points.splice(0);
-    this.drawing = false;
+    this.drawing = undefined;
+  }
+
+  updateLabels() {
+    this.svg.selectAll('label').remove();
+    for (const shape of this.shapes) {
+      for (let i = 1; i < shape.points.length; i++) {
+        const point1 = shape.points[i - 1];
+        const point2 = shape.points[i];
+        const midPoint = this.findMidPoint(point1, point2);
+        const text = this.findLength(point1, point2);
+        this.svg.insert('text', ':first-child')
+          .attr('x', midPoint.x)
+          .attr('y', midPoint.y)
+          .attr('text-anchor', 'middle')
+          .attr('class', 'label')
+          .text(text);
+      }
+    }
+  }
+
+  findMidPoint(point1, point2) {
+    return { x: (point1[0] + point2[0]) / 2, y: (point1[1] + point2[1]) / 2 };
+  }
+
+  findLength(point1, point2) {
+    var a = point1[0] - point2[0];
+    var b = point1[1] - point2[1];
+    return Math.round(Math.sqrt(a * a + b * b) * 100) / 100;
   }
 }
---
+++ src/app/shape.ts
@@ -0,0 +1,8 @@
+export class Shape {
+  public id: string;
+  public points: [];
+
+  constructor() {
+    this.points = [];
+  }
+}
